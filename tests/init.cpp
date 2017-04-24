94b0d30d1ab535cc92aa72b86bcef1084a305bdd
NeverMore27 <uragan.uragan.59@mail.ru>
2017-04-24 15:52:07 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -4,7 +4,7 @@ SCENARIO("NULL")
 {
 	int a[10] = { 1, 5, 6, 9 ,7, 2, 0 ,4, 5, 7 };
 	int b[10] = { 9, 7, 7, 6, 5 ,5,  4, 2, 1, 0 };
-	for (i; i < 10; i++)
+	for (int i=0; i < 10; i++)
 	{
 		REQUIRE(a[i] == b[i]);
 	}
