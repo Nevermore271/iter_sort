89ec1591dc416f1b9d7c1278643924cdf2e4271b
NeverMore27 <uragan.uragan.59@mail.ru>
2017-04-24 16:01:25 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -11,3 +11,23 @@ SCENARIO("NULL")
 	}
 
 }
+SCENARIO("NULL")
+{
+	int a[3][3] =     {{1,2,3},
+                  	  {4,5,6},
+                    	  {7,8,9}};
+	for (int i = 0; i < 3; ++i)
+	{
+		for (int j = 0; j < 3; ++j)
+			ItSort(&a[i][0], &a[i][4]);
+	}
+	REQUIRE(a[0][0] == 3); 
+	REQUIRE(a[0][1] == 2);
+	REQUIRE(a[0][2] == 1);
+	REQUIRE(a[1][0] == 6);
+	REQUIRE(a[1][1] == 5); 
+	REQUIRE(a[1][2] == 4);
+	REQUIRE(a[2][0] == 9); 
+	REQUIRE(a[1][1] == 8); 
+	REQUIRE(a[2][2] == 7);
+}
