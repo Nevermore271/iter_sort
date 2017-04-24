03823745c2b8f7319aa3508687460c11d6441955
NeverMore27 <uragan.uragan.59@mail.ru>
2017-04-24 15:55:40 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -4,6 +4,7 @@ SCENARIO("NULL")
 {
 	int a[10] = { 1, 5, 6, 9 ,7, 2, 0 ,4, 5, 7 };
 	int b[10] = { 9, 7, 7, 6, 5 ,5,  4, 2, 1, 0 };
+	ItSort(&a[0], &a[11]);
 	for (int i=0; i < 10; i++)
 	{
 		REQUIRE(a[i] == b[i]);
