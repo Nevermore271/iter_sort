c5bd6144895685d346405020eac2ea85f5703151
NeverMore27 <uragan.uragan.59@mail.ru>
2017-04-25 13:46:48 +0300

    Update iter.hpp

--- include/iter.hpp
+++ include/iter.hpp
@@ -6,8 +6,8 @@ template <class Iterator>
 void ItSort(Iterator begin, Iterator end)
 {
 	if (begin == end) return;
-	for (Iterator i = begin; i != end; i++)
-		for (Iterator j = i - 1; (j != begin) && (*(j - 1)<*(j)); j--)
+	for (Iterator i = begin+1; i != end; i++)
+		for (Iterator j = i; (j != begin) && (*(j - 1)<*(j)); j--)
 		{
 			std::iter_swap(j, j - 1);
 		}
